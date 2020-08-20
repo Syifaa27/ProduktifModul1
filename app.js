0378d7c6182aef20652823dffe44c83a92fe899e
Syifaa27 <syifa_auliya_27rpl@smktelkom-mlg.sch.id>
2020-08-20 15:55:35 +0700

    modul1

---
+++ app.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    new Vue({ });
+    <head>
+        <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+        <title>
+        Hello World
+        </title>
+        <body>
+             <div id="app">
+                <h1></h1>
+             </div>
+            <script src="app.js"></script>
+        </body>
+    </head>
+</html>
